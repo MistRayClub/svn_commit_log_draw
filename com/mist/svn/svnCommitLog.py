fbb265124efb5efbf25271633deaba75035bb38c
MistRay <542035625@qq.com>
2019-02-22 18:24:24 +0800

    修改缩进

---
+++ com/mist/svn/svnCommitLog.py
@@ -0,0 +1,16 @@
+# -*- coding: GBK -*-
+import os
+
+name = 'test'
+logs = os.system('G: && cd gjyWorkSpace/SMFdev/SMF && svn log --search ' + name + ' >c:/temp.txt')
+
+log = open("c:/temp.txt")
+lines = log.readlines()
+numer = 1
+unit = 1
+for line in lines:
+    if numer % 4 == 0:
+        print("".format(numer) + line)
+    if (line.startswith("Merged")):
+        continue
+    numer = numer + unit
